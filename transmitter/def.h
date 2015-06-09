3fe6d97eff3cc93bc9884815943df710f740d60f
theapi <petermclarke@gmail.com>
2015-06-09 21:50:41 +0100

    Fixed duplicate defines

--- transmitter/def.h
+++ transmitter/def.h
@@ -2,11 +2,11 @@
 #define DEF_H_
 
 // NRF24 payload key value pair keys 
-#define NRF24_KEY_VCC      0
-#define NRF24_KEY_HEADING  1
-#define NRF24_KEY_PITCH    2
-#define NRF24_KEY_ROLL     3
-#define NRF24_KEY_STATUS   4
+#define NRF24_KEY_VCC          0
+#define NRF24_KEY_ATT_HEADING  1
+#define NRF24_KEY_ATT_PITCH    2
+#define NRF24_KEY_ATT_ROLL     3
+#define NRF24_KEY_STATUS       4
 #define NRF24_KEY_THROTTLE 5
 #define NRF24_KEY_YAW      6
 #define NRF24_KEY_PITCH    7
